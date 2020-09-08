0f914304172cfe00a64ed283dd8363ab782ef710
Héctor M <hectormunozgarcia@gmail.com>
2020-09-08 20:02:41 +0100

    fetching games in gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,6 +38,14 @@ exports.createPages = async ({ graphql, actions }) => {
         }
       }
     }
+    allPrismicGames {
+      edges {
+        node {
+          uid
+          id
+        }
+      }
+    }
   }  
   `)
 
@@ -47,7 +55,7 @@ exports.createPages = async ({ graphql, actions }) => {
   }
 
   // Access query results via object destructuring
-  const { allPrismicPages, allPrismicPosts } = result.data
+  const { allPrismicPages, allPrismicPosts, allPrismicGames } = result.data
 
   // Create Page pages.
   const pageTemplate = path.resolve(`./src/templates/page.js`)
@@ -70,7 +78,7 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 
-  // Create Page pages.
+  // Create post pages.
   const postTemplate = path.resolve(`./src/templates/post.js`)
   // We want to create a detailed page for each page node.
   // The path field contains the relative original WordPress link
@@ -87,6 +95,27 @@ exports.createPages = async ({ graphql, actions }) => {
       // can query data specific to each page.
       path: edge.node.uid,
       component: slash(postTemplate),
+      context: edge.node,
+    })
+  })
+
+  // Create games pages.
+  const gameTemplate = path.resolve(`./src/templates/game.js`)
+  // We want to create a detailed page for each page node.
+  // The path field contains the relative original WordPress link
+  // and we use it for the uid to preserve url structure.
+  // The Page ID is prefixed with 'PAGE_'
+  allPrismicGames.edges.forEach(edge => {
+    // Gatsby uses Redux to manage its internal state.
+    // Plugins and sites can use functions like "createPage"
+    // to interact with Gatsby.
+    createPage({
+      // Each page is required to have a `path` as well
+      // as a template component. The `context` is
+      // optional but is often necessary so the template
+      // can query data specific to each page.
+      path: `/juego/${edge.node.uid}`,
+      component: slash(gameTemplate),
       context: edge.node,
     })
   })
---
+++ src/templates/game.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Game = (props) => {
+  return ( <h1>holi</h1> );
+}
+ 
+export default Game;
