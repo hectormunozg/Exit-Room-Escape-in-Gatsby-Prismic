9b10e9547e7a64b05d1d6cc018d5b46667968d91
Héctor M <hectormunozgarcia@gmail.com>
2020-09-08 20:10:10 +0100

    fetching game data from game.js

--- src/templates/game.js
+++ src/templates/game.js
@@ -1,6 +1,63 @@
-import React from 'react';
-const Game = (props) => {
-  return ( <h1>holi</h1> );
+import React from "react"
+import { graphql } from "gatsby"
+import { RichText } from "prismic-reactjs"
+
+import Nav from "../components/header/nav"
+
+export const query = graphql`
+  query MyGame($id: String) {
+    allPrismicGames(filter: { id: { eq: $id } }) {
+      edges {
+        node {
+          uid
+          id
+          data {
+            show_bookings
+            title {
+              raw
+            }
+            short_description {
+              raw
+            }
+            players {
+              raw
+            }
+            featured_image {
+              fluid {
+                src
+              }
+            }
+            duration {
+              raw
+            }
+            description {
+              raw
+            }
+            booking_embed {
+              raw
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const Game = props => {
+  console.log(props)
+  const gameTitle = props.data.allPrismicGames.edges[0].node.data.title.raw
+  const content = props.data.allPrismicGames.edges[0].node.data.description.raw
+  const image =
+    props.data.allPrismicGames.edges[0].node.data.featured_image.fluid.src
+
+  return (
+    <>
+      <Nav />
+      <img src={image} alt={gameTitle} />
+      <RichText render={gameTitle} />
+      <RichText render={content} />
+    </>
+  )
 }
- 
-export default Game;
+
+export default Game
